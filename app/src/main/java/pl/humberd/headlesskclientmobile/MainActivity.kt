38ce72dd80731ee38281bb5829ad8d15e32e191b
Humberd <humberd2@gmail.com>
2019-04-14 23:18:03 +0200

    Created status refresher. Learned fragments, styles, icons, and more.

--- app/src/main/java/pl/humberd/headlesskclientmobile/MainActivity.kt
+++ app/src/main/java/pl/humberd/headlesskclientmobile/MainActivity.kt
@@ -3,111 +3,121 @@ package pl.humberd.headlesskclientmobile
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
-import com.google.gson.GsonBuilder
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
-import pl.humberd.headlesskclientmobile.apis.Apis
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import java.time.Instant
+import android.widget.ImageView
+import android.widget.ProgressBar
+
+enum class WorkerStatfus(
+    val icon: Int,
+    val color: String,
+    val message: String
+) {
+    OK(R.drawable.ic_check_circle_black_24dp, "#00ff00", "Worker performs OK"),
+    FATAL_ERROR(R.drawable.ic_error_black_24dp, "#ff00ff", "Worker has been shut down and is waiting for intervention"),
+    NO_CONNECTION_WITH_WORKER(R.drawable.ic_phonelink_erase_black_24dp, "#757575", "Cannot connect to the Worker"),
+    NO_CONNECTION_WITH_BACKEND(R.drawable.ic_phonelink_off_black_24dp, "#757575", "Cannot connecto to the Backend")
+}
 
 class MainActivity : AppCompatActivity() {
-    val workerAPI = getWorkerApi()
+    lateinit var workerStatusIcon: ImageView;
+    lateinit var workerStatusProgressBar: ProgressBar;
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        swiperefresh.isRefreshing = true
+//        toolbar = findViewById(R.id.myToolbar)
+//        setSupportActionBar(toolbar)
 
-        swiperefresh.setOnRefreshListener {
-            getJobs {
-                initRecyclerView(it)
-            }
-        }
-
-        getJobs {
-            initRecyclerView(it)
-        }
+        workerStatusIcon = findViewById(R.id.workerStatusIcon)
+        workerStatusProgressBar = findViewById(R.id.workerStatusProgressBar)
 
 
-        Apis.status.getStatus()
-            .subscribe({
-                Log.d(TAG, it.status);
-            }, {
-                Log.e(TAG, it.message)
-            })
+//        swiperefresh.isRefreshing = true
+//        swiperefresh.setOnRefreshListener {
+//            getJobs {
+//                initRecyclerView(it)
+//            }
+//        }
+//
+//        getJobs {
+//            initRecyclerView(it)
+//        }
+//
+//
+//        Apis.status.getStatus()
+//            .subscribe({
+//                Log.d(TAG, it.status);
+//            }, {
+//                Log.e(TAG, it.message)
+//            })
 
-//        getToken();
     }
 
-    fun getToken() {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-
-                // Get new Instance ID token
-                val token = task.result?.token
-
-                // Log and toast
-                val msg = token
-                Log.d(TAG, msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getJobs(success: (List<Job>) -> Any) {
-        workerAPI.getJobs()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                success.invoke(it!!)
-            }, {
-                Log.e(TAG, "Some error occured ${it.message}")
-            }, {
-                swiperefresh.isRefreshing = false
-            })
-    }
-
-    companion object {
-        val TAG = "MainActivity"
-    }
-
-    private fun getWorkerApi(): WorkerAPI {
-        return object : WorkerAPI {
-            override fun getJobs(): Observable<List<Job>> {
-                return Observable.just(listOf(
-                    Job("work", Instant.now(), JobStatus.FAILED),
-                    Job("train", Instant.now(), JobStatus.SUCCESS),
-                    Job("fight", Instant.now(), JobStatus.UNKNOWN),
-                    Job("eat", Instant.now(), JobStatus.SUCCESS),
-                    Job("eat2", Instant.now(), JobStatus.SUCCESS),
-                    Job("eat2", Instant.now(), JobStatus.SUCCESS),
-                    Job("eat2", Instant.now(), JobStatus.SUCCESS),
-                    Job("buy", Instant.now(), JobStatus.SUCCESS)
-                ))
-            }
-        }
-    }
-
-    private fun initRecyclerView(jobs: List<Job>) {
-        val adapter = RecyclerViewAdapter(this, jobs)
-        findViewById<RecyclerView>(R.id.recycler_view).let {
-            it.adapter = adapter
-            it.layoutManager = LinearLayoutManager(this)
-        }
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_items, menu)
+//        return true
+//    }
+//
+//    fun getToken() {
+//        FirebaseInstanceId.getInstance().instanceId
+//            .addOnCompleteListener(OnCompleteListener { task ->
+//                if (!task.isSuccessful) {
+//                    Log.w(TAG, "getInstanceId failed", task.exception)
+//                    return@OnCompleteListener
+//                }
+//
+//                // Get new Instance ID token
+//                val token = task.result?.token
+//
+//                // Log and toast
+//                val msg = token
+//                Log.d(TAG, msg)
+//                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+//            })
+//    }
+//
+//    @SuppressLint("CheckResult")
+//    fun getJobs(success: (List<Job>) -> Any) {
+//        workerAPI.getJobs()
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                success.invoke(it!!)
+//            }, {
+//                Log.e(TAG, "Some error occured ${it.message}")
+//            }, {
+//                swiperefresh.isRefreshing = false
+//            })
+//    }
+//
+//    companion object {
+//        val TAG = "MainActivity"
+//    }
+//
+//    private fun getWorkerApi(): WorkerAPI {
+//        return object : WorkerAPI {
+//            override fun getJobs(): Observable<List<Job>> {
+//                return Observable.just(
+//                    listOf(
+//                        Job("work", Instant.now(), JobStatus.FAILED),
+//                        Job("train", Instant.now(), JobStatus.SUCCESS),
+//                        Job("fight", Instant.now(), JobStatus.UNKNOWN),
+//                        Job("eat", Instant.now(), JobStatus.SUCCESS),
+//                        Job("eat2", Instant.now(), JobStatus.SUCCESS),
+//                        Job("eat2", Instant.now(), JobStatus.SUCCESS),
+//                        Job("eat2", Instant.now(), JobStatus.SUCCESS),
+//                        Job("buy", Instant.now(), JobStatus.SUCCESS)
+//                    )
+//                )
+//            }
+//        }
+//    }
+//
+//    private fun initRecyclerView(jobs: List<Job>) {
+//        val adapter = RecyclerViewAdapter(this, jobs)
+//        findViewById<RecyclerView>(R.id.recycler_view).let {
+//            it.adapter = adapter
+//            it.layoutManager = LinearLayoutManager(this)
+//        }
+//    }
 }
---
+++ app/src/main/java/pl/humberd/headlesskclientmobile/WorkerStatus.kt
@@ -0,0 +1,157 @@
+package pl.humberd.headlesskclientmobile
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
+import pl.humberd.headlesskclientmobile.apis.Apis
+import pl.humberd.headlesskclientmobile.apis.StatusDto
+import java.util.concurrent.TimeUnit
+
+enum class WorkerStatusEnum(
+    val icon: Int,
+    val color: String,
+    val message: String,
+    var errorMessage: String = ""
+) {
+
+    OK(R.drawable.ic_check_circle_black_24dp, "#00ff00", "Worker performs OK"),
+    FATAL_ERROR(R.drawable.ic_error_black_24dp, "#ff00ff", "Worker has been shut down and is waiting for intervention"),
+    WORKER_DISCONNECTED(R.drawable.ic_phonelink_erase_black_24dp, "#757575", "Worker Disconnected"),
+    BACKEND_DISCONNECTED(R.drawable.ic_phonelink_off_black_24dp, "#757575", "Backend Disconnected"),
+    UNKNOWN_STATUS(R.drawable.ic_battery_unknown_black_24dp, "#757575", "Unknown response:");
+
+    fun displayToast(context: Context?) {
+        val printableMessage = "${message} ${errorMessage}"
+        Toast.makeText(context, printableMessage, Toast.LENGTH_LONG)
+            .show()
+    }
+}
+
+class WorkerStatus : Fragment() {
+    val refreshSubject = PublishSubject.create<Any>()
+    val destroySubject = PublishSubject.create<Any>()
+
+    lateinit var workerStatusIcon: ImageView;
+    lateinit var workerStatusProgressBar: ProgressBar;
+
+    private lateinit var currentStatus: WorkerStatusEnum;
+
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        workerStatusIcon = view.findViewById(R.id.workerStatusIcon)
+        workerStatusProgressBar = view.findViewById(R.id.workerStatusProgressBar)
+
+        workerStatusIcon.setOnClickListener {
+            currentStatus.displayToast(context)
+        }
+
+        workerStatusIcon.setOnLongClickListener {
+            refresh()
+            return@setOnLongClickListener true
+        }
+
+        subscribe()
+        refresh()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        destroySubject.onNext(true);
+
+    }
+
+    fun subscribe(): Disposable {
+        return refreshSubject
+            .takeUntil(destroySubject)
+            .switchMap { Observable.interval(0, 20, TimeUnit.SECONDS) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Log.d(TAG, "Request trigger")
+                workerStatusProgressBar.visibility = View.VISIBLE
+                workerStatusIcon.visibility = View.GONE
+            }
+            .switchMap {
+                Apis.status.getStatus()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnNext { onStatusReceive(it) }
+                    .doOnError { onStatusError(it) }
+                    .onErrorResumeNext(Observable.empty())
+            }
+            .subscribe({
+                Log.d(TAG, "Request SUCCESS")
+            }, {
+                Log.e(TAG, it.message)
+            })
+    }
+
+    fun refresh() {
+        refreshSubject.onNext(true)
+    }
+
+    fun onStatusReceive(statusDto: StatusDto) {
+        when (statusDto.status.toUpperCase()) {
+            "OK" -> updateStatus(WorkerStatusEnum.OK)
+            "FATAL_ERROR" -> updateStatus(WorkerStatusEnum.FATAL_ERROR)
+            "DISCONNECTED" -> updateStatus(WorkerStatusEnum.WORKER_DISCONNECTED)
+            else -> {
+                val status = WorkerStatusEnum.UNKNOWN_STATUS
+                status.errorMessage = statusDto.status
+                updateStatus(status)
+
+                status.displayToast(context)
+            }
+        }
+    }
+
+    fun onStatusError(error: Throwable) {
+        val status = WorkerStatusEnum.BACKEND_DISCONNECTED;
+        val errMessage = error.message
+        status.errorMessage = if (errMessage !== null) errMessage else "No error message"
+
+        updateStatus(status)
+
+        status.displayToast(context)
+
+        Log.e(TAG, error.message)
+    }
+
+    fun updateStatus(status: WorkerStatusEnum) {
+        currentStatus = status;
+        workerStatusProgressBar.visibility = View.GONE
+        workerStatusIcon.also {
+            it.visibility = View.VISIBLE
+            it.setColorFilter(Color.parseColor(status.color))
+            it.setImageResource(status.icon)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_worker_status, container, false)
+    }
+
+
+    companion object {
+        val TAG = "WorkerStatus"
+    }
+}
